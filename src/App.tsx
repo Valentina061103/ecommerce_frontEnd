45a9ea0c76df6956e0ea2d2ce576f3d518b67332
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-05-11 21:37:29 -0300

    pagina de carrito con estilos listos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { Cart } from "./pages/Cart/Cart";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
     </Router>
   );
