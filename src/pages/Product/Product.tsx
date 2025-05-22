e6c899b0a85f53ad96037780b0b6eef53e73c7c8
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-05-22 12:47:29 -0300

    css en proceso, disposicio del contenido lista

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -13,13 +13,27 @@ export const Product = () => {
     }
 
     return (
-        <div className={styles.ProductDetailContainer}>
-            <img src={producto.imagen} alt={producto.nombre} className={styles.ProductImage} />
-            <div className={styles.ProductInfo}>
-                <h1>{producto.nombre}</h1>
-                <p className={styles.ProductPrice}>${producto.precio.toLocaleString()}</p>
-                <p>{producto.marca} - {producto.color}</p>
-                <button className={styles.AddToCartButton}>Agregar al Carrito</button>
+        <div className={styles.container_content}>
+            <div className={styles.ProductDetailContainer}>
+                <div className={styles.product_imagesSection}>
+                    <div className={styles.product_imagesSection_imagen}>1</div>
+                    <div className={styles.product_imagesSection_imagen}>2</div>
+                    <div className={styles.product_imagesSection_imagen}>3</div>
+                    <div className={styles.product_imagesSection_imagen}>4</div>
+                </div>
+                <div className={styles.product_imagenPricipalSection}>
+                    <img src={producto.imagen} alt={producto.nombre} className={styles.product_imagenPricipalSection_imagen} />
+                </div>
+                <div className={styles.product_infoSection}>
+                    <h1>{producto.nombre}</h1>
+                    <p>{producto.marca} - {producto.color}</p>
+                    <p className={styles.ProductPrice}>${producto.precio.toLocaleString()}</p>
+
+                    <div className={styles.product_infoSection_talles}>
+                        <p>Seleccionar un talle</p>
+                    </div>
+                    <button className={styles.AddToCartButton}>Agregar al Carrito</button>
+                </div>
             </div>
         </div>
     );
