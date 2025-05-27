6626472c3ff535c8170ae15d7284b6d0fe754904
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-05-27 16:44:44 -0300

    Creación del footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Footer.module.css"
+
+export const Footer = () => {
+    return (
+        <div className={styles.container_footer}>
+            <h2>Sumate a la comunidad</h2>
+            <p>Mendoza-Argentina</p>
+        </div>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css"
 import CarouselCategory from "../../components/CarouselCategory/CarouselCategory";
 import fondo from '../../assets/imagenfondo.jpg';
+import { Footer } from "../../components/Footer/Footer";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -23,6 +24,8 @@ const Home = () => {
 
       <h3 className={styles.conteiner_corouselProducts}>Aca va un carrosusel de productos</h3>
       <Slider />
+
+      <Footer/>
     </div>
   );
 };
