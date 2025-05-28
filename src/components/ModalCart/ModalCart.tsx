5904f8b9988eb9b7d85b4134bf51a1310e237c5b
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-05-27 23:00:42 -0300

    css de modalCart listo, con botones funcionando

--- src/components/ModalCart/ModalCart.tsx
+++ src/components/ModalCart/ModalCart.tsx
@@ -26,23 +26,36 @@ export const ModalCart = ({ onClose }: ModalCartProps) => {
         onClose();       // Cierra el modal
         navigate('/Cart'); // Navega a la página del carrito
     }
-    
+
+    const subtotal = Shoes.reduce((acc, shoe) => acc + shoe.price, 0);
+
     return (
         <div className={styles.modal} onClick={onClose}>
             <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-                <span className="material-symbols-outlined" onClick={onClose}>arrow_back</span>
-                <h2>Mi compra</h2>
+                <div className={styles.container_titulo}>
+                    <span className="material-symbols-outlined" onClick={onClose}>arrow_back</span>
+                    <h2>Mi compra</h2>
+                </div>
                 <div className={styles.container_contentCard}>
                     <div className={styles.container_cardsProducts}>
                         {Shoes.map((shoe, index) => (
                             <CardProductCartModal key={index} shoe={shoe} />
                         ))}
                     </div>
-                    <div className={styles.container_price}>
-                    </div>
-                    <button onClick={onClose}>Seguir comprando</button>
-                    <button onClick={handleGoToCart}>Ver carrito</button>
                 </div>
+                <div className={styles.container_actions}>
+                        <div className={styles.container_price}>
+                            <p>Total</p>
+                            <p>${subtotal.toLocaleString()}</p>
+                        </div>
+                        <button className={styles.container_button_black}>Finalizar compra</button>
+                        <div className={styles.container_buttons}>
+                            <p onClick={onClose}><span className="material-symbols-outlined">chevron_left</span>
+                            Seguir comprando</p>
+                            <p onClick={handleGoToCart}>Ver carrito
+                            <span className="material-symbols-outlined">chevron_right</span></p>
+                        </div>
+                    </div>
             </div>
         </div>
     )
