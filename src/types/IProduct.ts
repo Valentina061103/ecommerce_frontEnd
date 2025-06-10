dcf175c94a4ce3ff4ea4de50153dfc368b55c1ab
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-06-10 11:29:37 -0300

    carrito funcionando

--- src/types/IProduct.ts
+++ src/types/IProduct.ts
@@ -20,25 +20,25 @@ export interface Categoria {
     nombre: string;
 }
 
-export interface Detalle {
-    id: number;
-    estado: boolean;
-    color: string;
-    marca: string;
-    stock: number;
-    precio: Precio;
-    talle: Talle | null;
-    imagenes: Imagen[];
-}
+// export interface Detalle {
+//     id: number;
+//     estado: boolean;
+//     color: string;
+//     marca: string;
+//     stock: number;
+//     precio: Precio;
+//     talle: Talle | null;
+//     imagenes: Imagen[];
+// }
 
-export interface Producto {
-    id: number;
-    nombre: string;
-    sexo: 'MASCULINO' | 'FEMENINO' | 'UNISEX';
-    tipoProducto: 'ZAPATILLA' | 'REMERA' | 'BUZO' | 'SHORT' | 'PANTALON' | 'ACCESORIO';
-    categoria: Categoria | null;
-    detalles: Detalle[];
-}
+// export interface Producto {
+//     id: number;
+//     nombre: string;
+//     sexo: 'MASCULINO' | 'FEMENINO' | 'UNISEX';
+//     tipoProducto: 'ZAPATILLA' | 'REMERA' | 'BUZO' | 'SHORT' | 'PANTALON' | 'ACCESORIO';
+//     categoria: Categoria | null;
+//     detalles: Detalle[];
+// }
 
 
 //agregar tipo de tproducto
@@ -71,4 +71,37 @@ export interface ProductoRequestDTO {
     marca: string;
     imagenesUrls: string[];
     detalles: DetalleRequestDTO[];
+}
+
+export interface Producto {
+    id: number;
+    nombre: string;
+    sexo: 'MASCULINO' | 'FEMENINO' | 'UNISEX';
+    tipoProducto: 'ZAPATILLA' | 'REMERA' | 'BUZO' | 'SHORT' | 'PANTALON' | 'ACCESORIO';
+    categoria: {
+        id: number;
+        nombre: string;
+    };
+    detalles: Detalle[];
+}
+
+export interface Detalle {
+    id: number;
+    estado: boolean;
+    color: string;
+    marca: string;
+    stock: number;
+    precio: {
+        id: number;
+        precioCompra: number;
+        precioVenta: number;
+    };
+    talle: {
+        id: number;
+        talle: string;
+    };
+    imagenes: Array<{
+        id: number;
+        url: string;
+    }>;
 }
